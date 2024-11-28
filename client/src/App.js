1b63f82efbd6dcc9a4825d055f94912a582d1d8a
Ushal Shrestha <ushal_bindukar@ymail.com>
2024-11-28 13:32:29 +0545

    button design changed and naration added

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import logo from './photo/logo.png';
+import FlagNP from './photo/FlagNP.gif'
 import './App.css';
 import TicTacToe from './main/TicTacToe';
 
@@ -8,7 +9,8 @@ const App = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          WELCOME <br /> Play Tic Tac Toe Against AI (Artificial Inteligence)
+          WELCOME <br /> Play Tic Tac Toe Against AI (Artificial Inteligence)<br />Game By : USHAL SHRESTHA
+          <img src={FlagNP} alt='flag' className='flag' />
         </p>
         <TicTacToe />
       </header>
